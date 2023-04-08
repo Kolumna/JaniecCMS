ded6d5da47acc5456b59896bf9f1d23761ce4b4c
K0lumna <michaljaniec12@wp.pl>
2023-04-08 21:16:42 +0200

    base

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { Alert, Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <Container className="mt-4">
+      <Alert variant="primary">JaniecCMS</Alert>
+    </Container>
+  );
+}
+
+export default App;
